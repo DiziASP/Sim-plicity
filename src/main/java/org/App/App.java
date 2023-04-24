368992034fd9fd2b2e606957fc23ea7d82a41536
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-04-24 13:32:55 +0700

    Feat: Initial Commit

---
+++ src/main/java/org/App/App.java
@@ -0,0 +1,12 @@
+package org.App;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App {
+    public App(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        new App();
+    }
+}
